f7134ca1952968d61729b8f1ac7f61390a3075f2
thatkunaaal <thatkunaal@gmail.com>
2025-04-23 09:02:09 +0530

    top/all restaurant

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,23 +1,36 @@
 import RestaurantCard from "./Restaurant";
-import {resList} from "../utils/mockData";
+import { resList } from "../utils/mockData";
+import { useState } from "react";
 
-const Body = () => (
-  <div className="Body-containter">
-    <div className="search-box">
-      <input placeholder="Enter the restaurant to search" />
-      <button>Search</button>
+const Body = () => {
+  const [toggle,setToggle] = useState(true);
+  const [listOfRestaurant,setListOfRestaurant] = useState(resList);
+  const filterTopRestaurant = () => {
+    if(toggle){
+      const filteredRestaurant = listOfRestaurant.filter(restaurant => 
+        restaurant.info.avgRating >= 4.5
+      )
+      setListOfRestaurant(filteredRestaurant);
+    }
+    else{
+      setListOfRestaurant(resList);
+    }
+    setToggle(!toggle);
+  }
+  return (
+    <div className="Body-containter">
+      <div className="search-box">
+        <input placeholder="Enter the restaurant to search" />
+        <button>Search</button>
+        <button className="top-restaurant-btn" onClick={filterTopRestaurant}>{toggle?"Top Restaurant":"All Restaurant"}</button>
+      </div>
+      <div className="restaurant-container">
+        {listOfRestaurant.map((item) => {
+          return <RestaurantCard key={item.info.id} resData={item} />;
+        })}
+      </div>
     </div>
-    <div className="restaurant-container">
-      {
-        resList.map((item)=>{
-            return  (<RestaurantCard key={item.info.id}
-            resData = {item}
-           />)
-        })
-      }
-     
-    </div>
-  </div>
-);
+  );
+};
 
 export default Body;
